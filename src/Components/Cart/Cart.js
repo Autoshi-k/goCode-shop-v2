ebb68f684c32b3a676071950bdddda099d803bac
Autoshi-k <autoshigit@gmail.com>
2021-11-13 14:18:26 +0200

    cart style

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -8,6 +8,8 @@ import { CartContext } from '../../context/CartContext';
 import Product from '../Product/Product';
 import InCart from '../InCart/InCart';
 
+import './Cart.css';
+
 function Cart() {
 
   const cart = useContext(CartContext);
@@ -17,11 +19,13 @@ function Cart() {
   return (
     <List>
       <div className="cart" style={{width: 500}}>
-        <h2>Cart</h2>
-        <h4>You Have 2 Items In Cart</h4>
+        <h2 className="cart-title">Cart</h2>
+        <h4 className="cart-information">You Have 2 Items In Cart</h4>
         <Divider />
-        { Object.keys(cart.cart).map((index, key) => <InCart index={ index } key ={ key } amount={ cart.cart[index] }/>) }
-
+        <div className="list-in-cart">
+          { Object.keys(cart.cart).map((index, key) => <InCart index={ index } key ={ key } amount={ cart.cart[index] }/>) }
+        </div>
+        <div className="checkout-btn">proceed to checkout</div>
       </div>
     </List>
   );
